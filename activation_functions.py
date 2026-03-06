5cf6d14fe600375f1440fd6c29d7830a7ba9cbb2
aaronp64 <aaronp.code@gmail.com>
2026-03-06 13:17:36 -0500

    Shortened NDArray type hinting

--- activation_functions.py
+++ activation_functions.py
@@ -1,20 +1,20 @@
 from abc import ABC, abstractmethod
 
 import numpy as np
-import numpy.typing as npt
+from numpy.typing import NDArray
 
 class ActivationFunction(ABC):
     @abstractmethod
-    def apply(self, values: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+    def apply(self, values: NDArray[np.float64]) -> NDArray[np.float64]:
         pass
 
     @abstractmethod
-    def derivative(self, values: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+    def derivative(self, values: NDArray[np.float64]) -> NDArray[np.float64]:
         pass
 
 class ReLU(ActivationFunction):
-    def apply(self, values: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+    def apply(self, values: NDArray[np.float64]) -> NDArray[np.float64]:
         return np.maximum(0, values)
 
-    def derivative(self, values: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+    def derivative(self, values: NDArray[np.float64]) -> NDArray[np.float64]:
         return (values > 0).astype(np.float64)
--- loss_functions.py
+++ loss_functions.py
@@ -1,30 +1,30 @@
 from abc import ABC, abstractmethod
 
 import numpy as np
-import numpy.typing as npt
+from numpy.typing import NDArray
 
 class LossFunction(ABC):
     @abstractmethod
     def apply(
-            self, actual: npt.NDArray[np.float64],
-            predicted: npt.NDArray[np.float64]) -> np.float64:
+            self, actual: NDArray[np.float64],
+            predicted: NDArray[np.float64]) -> np.float64:
         pass
 
     @abstractmethod
     def derivative(
-            self, actual: npt.NDArray[np.float64],
-            predicted: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+            self, actual: NDArray[np.float64],
+            predicted: NDArray[np.float64]) -> NDArray[np.float64]:
         pass
 
 class MeanSquaredError(LossFunction):
     def apply(
-            self, actual: npt.NDArray[np.float64],
-            predicted: npt.NDArray[np.float64]) -> np.float64:
-        squared: npt.NDArray[np.float64] = np.square(predicted - actual)
-        batch_totals: npt.NDArray[np.float64] = np.sum(squared, axis=1)
+            self, actual: NDArray[np.float64],
+            predicted: NDArray[np.float64]) -> np.float64:
+        squared: NDArray[np.float64] = np.square(predicted - actual)
+        batch_totals: NDArray[np.float64] = np.sum(squared, axis=1)
         return np.float64(np.mean(batch_totals))
 
     def derivative(
-            self, actual: npt.NDArray[np.float64],
-            predicted: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+            self, actual: NDArray[np.float64],
+            predicted: NDArray[np.float64]) -> NDArray[np.float64]:
         return (2 / actual.shape[0]) * (predicted - actual)
