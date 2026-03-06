ff4b4c306ea3d78871c2547e87f9b4ac4c729c03
aaronp64 <aaronp.code@gmail.com>
2026-03-06 14:07:33 -0500

    Started setting up backward pass logic

--- layers.py
+++ layers.py
@@ -1,8 +1,7 @@
 from abc import ABC, abstractmethod
-from typing import Self
 
 import numpy as np
-import numpy.typing as npt
+from numpy.typing import NDArray
 
 from activation_functions import ActivationFunction
 
@@ -15,7 +14,7 @@ class Layer(ABC):
         return self._size
 
     @abstractmethod
-    def forward_pass(self, input_data: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+    def forward_pass(self, input_data: NDArray[np.float64]) -> NDArray[np.float64]:
         pass
 
 class DenseLayer(Layer):
@@ -25,17 +24,31 @@ class DenseLayer(Layer):
         super().__init__(size)
 
         # TODO: Move weight initialization to separate functions to be passed in
-        self._weights: npt.NDArray[np.float64] = np.random.randn(input_size, size) * 0.01
-        self._biases: npt.NDArray[np.float64] = np.zeros((1, size))
+        self._weights: NDArray[np.float64] = np.random.randn(input_size, size) * 0.01
+        self._biases: NDArray[np.float64] = np.zeros((1, size))
         self._activation_function: ActivationFunction = activation_function
 
         # Cache for forward pass data
-        self._input: npt.NDArray[np.float64] | None = None
-        self._output: npt.NDArray[np.float64] | None = None
+        self._input: NDArray[np.float64] = np.array([])
+        self._pre_output: NDArray[np.float64] = np.array([])
 
-    def forward_pass(self, input_data: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+    def forward_pass(self, input_data: NDArray[np.float64]) -> NDArray[np.float64]:
         self._input = input_data
 
-        pre_output = np.dot(self._input, self._weights) + self._biases
-        self._output = self._activation_function.apply(pre_output)
-        return self._output
+        self._pre_output = np.dot(self._input, self._weights) + self._biases
+        return self._activation_function.apply(self._pre_output)
+
+    def backward_pass(
+            self, output_gradient: NDArray[np.float64],
+            learning_rate: float) -> NDArray[np.float64]:
+        chain_gradient: NDArray[np.float64] = (
+                output_gradient * self._activation_function.derivative(self._pre_output))
+
+        input_gradient: NDArray[np.float64] = np.dot(chain_gradient, self._weights.T)
+        weight_gradient: NDArray[np.float64] = np.dot(self._input.T, chain_gradient)
+        bias_gradient: NDArray[np.float64] = np.sum(chain_gradient, axis=0, keepdims=True)
+
+        self._weights -= weight_gradient * learning_rate
+        self._biases -= bias_gradient * learning_rate
+
+        return input_gradient
